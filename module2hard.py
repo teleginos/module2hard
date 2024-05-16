11834dd83c0b3adeecef28a6bdf9d3ad9c4399f4
Telegin Oleg <teleginos@mail.ru>
2024-05-16 22:55:03 +0300

    add func random_number

--- module2hard.py
+++ module2hard.py
@@ -1,10 +1,20 @@
+import random
+
+
 def description_code(number):
-    result = ''
+    result = ""
     for i in range(1, (number // 2) + 1):
-        for j in range(i+1, number):
+        for j in range(i + 1, number):
             if number % (i + j) == 0:
                 result += str(i) + str(j)
     return int(result)
 
 
-print(description_code(20))
+def random_number():
+    number = random.randint(3, 20)
+    print(f"|{'-'*65}|\n|Случайное число в первом поле: {number}\n|{'-'*65}|")
+
+    return number
+
+
+print(f"|Пароль к данному числу: {description_code(random_number())}\n|{'-'*65}|")
